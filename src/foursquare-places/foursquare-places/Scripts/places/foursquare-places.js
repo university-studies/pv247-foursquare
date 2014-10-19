a1710f242f5e6c29e1dddaf76803f9ebe6f8f46c
oprazak <ondpraz@gmail.com>
2014-10-19 14:06:51 +0200

    maps directive initial version

--- src/foursquare-places/foursquare-places/Scripts/places/foursquare-places.js
+++ src/foursquare-places/foursquare-places/Scripts/places/foursquare-places.js
@@ -3,3 +3,22 @@
 app.controller('MapController', ['$scope', function ($scope) {
     $scope.greeting = "hello from angular!";
 }]);
+
+
+app.directive('map', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div></div>',
+        link: function (scope, element, attrs) {
+            console.log(element);
+
+            var mapOptions = {
+                zoom: 15,
+                center: new google.maps.LatLng(40.69847032728747, -73.9514422416687)
+            }
+
+            map = new google.maps.Map(document.getElementById(attrs.id), mapOptions);
+        }
+    }
+})
