c7511c3dd62b181212b2905ced8fae169858a28a
Maros <sharksyk@gmail.com>
2014-10-26 20:12:27 +0100

    Oauth2 test1

--- src/foursquare-places/Controllers/HomeController.cs
+++ src/foursquare-places/Controllers/HomeController.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using foursquare_places.Models;
+using FourSquare.SharpSquare.Core;
+using FourSquare.SharpSquare.Entities;
 
 namespace foursquare_places.Controllers
 {
     public class HomeController : Controller
     {
+        static string clientId = "T4JTQ1SK5CDOD4HSBA00HIZRTR1H04JP43RXCSIOLE0IDWRK";
+        static string clientSecret = "BSKKMLGAFHZ1VSUL4FL5CP0D31QIGJIZV0WPNII3KQUZQVLG";
+        static string redirectUri = "http://foursquare-places.azurewebsites.net/";
+        static SharpSquare sharpSquare;
+
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult UserClicksAuthenticate()
+        {
+            //var redirectUri = Request.Url.Authority + this.Url.Action("AuthorizeCallback", new { userCode = "userCode" });
+            
+            //localhost test
+            //string redirectUri = "http://localhost:52978/";
+            sharpSquare = new SharpSquare(clientId, clientSecret);
+            var authUrl = sharpSquare.GetAuthenticateUrl(redirectUri);
+
+            return new RedirectResult(authUrl, permanent: false);
         }
 
         /*public ActionResult About()
