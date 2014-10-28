5c5dc8e407b44b798d8775b9fa7fe94597c66855
Maros <sharksyk@gmail.com>
2014-10-28 19:49:02 +0100

    Oauth test 4 addendum

--- src/foursquare-places/App_Start/RouteConfig.cs
+++ src/foursquare-places/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace foursquare_places
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "AuthorizeCallback",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "AuthorizeCallback", id = UrlParameter.Optional });
+
         }
     }
 }
