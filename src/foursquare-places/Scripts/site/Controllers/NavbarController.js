1e2c04fc41c3d0747a01dfe7070d8d37b2afe60e
oprazak <ondpraz@gmail.com>
2014-11-21 17:05:30 +0100

    removed razor completely

--- src/foursquare-places/Scripts/site/Controllers/NavbarController.js
+++ src/foursquare-places/Scripts/site/Controllers/NavbarController.js
@@ -1,11 +1,15 @@
 ﻿angular.module('FoursquareModule').controller('NavbarController', 
-    ['$scope', 'MarkerUtils',
-        function ($scope, MarkerUtils) {
+    ['$scope', '$window', 'MarkerUtils',
+        function ($scope, $window,  MarkerUtils) {
 
             $scope.toggleCategory = function (category) {
                 $scope.categories[category] = !$scope.categories[category];
                 
                 MarkerUtils.filterMarkers($scope.markers, $scope.categories, $scope.map);
-            };       
+            };
+
+            $scope.transitionHome = function () {
+                $window.location.href = '/'
+            }
     }]
 )
