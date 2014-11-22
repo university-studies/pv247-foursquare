9ad2c21300e7f4ea75caf69cc2e8c67a16a061ce
oprazak <ondpraz@gmail.com>
2014-11-22 19:10:30 +0100

    added dependencies

--- src/foursquare-places/Scripts/spec/MarkerFormatterSpec.js
+++ src/foursquare-places/Scripts/spec/MarkerFormatterSpec.js
@@ -1,31 +1,30 @@
 ﻿describe("Factory: MarkerFormatter", function () {
     var venue, location, map, categories, marker;
 
-    //beforeEach(module('FoursquareModule'));
-    beforeEach(function () { module('FoursquareModule');} );
+    
+    beforeEach(module('FoursquareModule'));
 
-    /*beforeEach(inject(function ($injector) {
+    beforeEach(inject(function ($injector) {
 
         MarkerFormatter = $injector.get('MarkerFormatter');
-        map = { name: 'mocked gmap' };
+        //map = new google.maps.Map();
         location = {Latitude: '58.68', Longitude: '48.24'}
        
-    }));*/
+    }));
 
     it("should return marker with position, id, title, category, clickable", function () {
-        //venue = {
-           /* Id: 1,
+        venue = {
+            Id: 1,
             Name: 'mocked venue',
             Location: location,
             Category: 'Unknown',
-            CheckinsCount: 150,*/
+            CheckinsCount: 150,
 
-        //};
+        };
         
-        //categories = { 'Unknown': true };
+        categories = { 'Unknown': true };
 
         //marker = MarkerFormatter.markVenue(venue, map, categories);
-
         //expect(marker.clickable).toBe(true);
 
     });
