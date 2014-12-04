ab7bccee1c8572d97fdce194781ef7559d7fa8ad
oprazak <ondpraz@gmail.com>
2014-12-04 13:29:52 +0100

    filters optimized for mobile, shorter footer text

--- src/foursquare-places/Scripts/site/Controllers/NavbarController.js
+++ src/foursquare-places/Scripts/site/Controllers/NavbarController.js
@@ -4,11 +4,20 @@
     var NavbarController = function ($scope, $window, MarkerUtils) {
 
         $scope.toggleCategory = function (category) {
-            $scope.categories[category] = !$scope.categories[category];            
+            $scope.navCollapsed = true
+            for (var cat in $scope.categories) {                
+                if (cat == category) {
+                    $scope.categories[cat] = true;
+                } else {
+                    $scope.categories[cat] = false;
+                }                
+            }
+                       
             MarkerUtils.filterMarkers($scope.markers, $scope.categories, $scope.map);
         };        
 
-        $scope.toCurrentPosition = function () {            
+        $scope.toCurrentPosition = function () {
+            $scope.navCollapsed = true;
             google.maps.event.clearListeners($scope.map, 'center_changed');
             $scope.map.setCenter(new google.maps.LatLng($scope.currentPosition.latitude, $scope.currentPosition.longitude));
             $scope.addMapCenterChangedListener();
