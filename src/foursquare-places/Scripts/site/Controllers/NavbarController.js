7a2ecfb55de2ff99a1aacde82c47fe8adcd6f774
oprazak <ondpraz@gmail.com>
2014-12-04 14:15:03 +0100

    initial filter styling

--- src/foursquare-places/Scripts/site/Controllers/NavbarController.js
+++ src/foursquare-places/Scripts/site/Controllers/NavbarController.js
@@ -3,8 +3,11 @@
     var injectParams = ['$scope', '$window', 'MarkerUtils'];
     var NavbarController = function ($scope, $window, MarkerUtils) {
 
+        var initNavbarStyle = true;
+
         $scope.toggleCategory = function (category) {
-            $scope.navCollapsed = true
+            $scope.navCollapsed = true;
+            initNavbarStyle = false;
             for (var cat in $scope.categories) {                
                 if (cat == category) {
                     $scope.categories[cat] = true;
@@ -18,10 +21,24 @@
 
         $scope.toCurrentPosition = function () {
             $scope.navCollapsed = true;
+            initNavbarStyle = false;
             google.maps.event.clearListeners($scope.map, 'center_changed');
             $scope.map.setCenter(new google.maps.LatLng($scope.currentPosition.latitude, $scope.currentPosition.longitude));
             $scope.addMapCenterChangedListener();
         };
+
+        $scope.chooseStyle = function (category) {
+            if (initNavbarStyle) {
+                return false;
+            } else {
+                if ($scope.categories[category]) {                    
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
     };
 
     NavbarController.$inject = injectParams;
